3866e73dcef3213267b124aedf9458128b89af73
wakanamorooka <105894768+wakanamorooka@users.noreply.github.com>
2022-05-29 00:57:17 +0900

    1-5課題提出
    1-5関数
    課題提出

---
+++ 1-5/main.js
@@ -0,0 +1,22 @@
+function createJuice(fruits) {
+
+    
+    console.log(fruits + "を受け取りました。ジュースにして返します");
+    
+    return fruits + 'ジュース' + 'が届きました';
+}
+
+console.log(createJuice('みかん'));
+   
+
+
+
+
+  
+
+
+
+
+
+
+
