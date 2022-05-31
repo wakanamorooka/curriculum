eb54454c99961f83ff90dab59b2b7bae8457df18
wakanamorooka <105894768+wakanamorooka@users.noreply.github.com>
2022-06-01 00:30:14 +0900

    1-5,1-6課題再提出
    JSの課題再提出

--- 1-5/main.js
+++ 1-5/main.js
@@ -1,15 +1,9 @@
 function createJuice(fruits) {
-
-    
-    console.log(fruits + "を受け取りました。ジュースにして返します");
-    
+  console.log(fruits + "を受け取りました。ジュースにして返します");
     return fruits + 'ジュース' + 'が届きました';
 }
-
+    let orangeJuice = createJuice('みかん'); 
 console.log(createJuice('みかん'));
-   
-
-
 
 
   
--- 1-6/main.js
+++ 1-6/main.js
@@ -1,16 +1,12 @@
 //問1：10、15、20、25を含む:/
 //配列の変数scoresを用意して、画像のように偶数だけを出力してください。
 
-let scores = [10,15,20,25];
-console.log(scores);
-scores = 10;
-if (scores % 2 === 0) {
-    console.log(scores + "は偶数です");
- }
-scores= 20;
- if (scores % 2 === 0) {
-    console.log(scores + "は偶数です");
- }
+let scores =[10,15,20,25];
+for(let i = 0; i < scores.length; i++)
+   if (i % 2 === 0){
+    console.log( scores[i] + "は偶数です");
+}
+
 
  //問2：ガソリンとナンバーのプロパティを持った「carオブジェクト」を作成して、ガソリンとナンバーを出力してください。
 
@@ -18,8 +14,7 @@ scores= 20;
      gass: 20.5,
      num: 1234 
  };
- console.log(cars.gass);
- console.log(cars.num)
+ 
  console.log("ガソリンは" + cars.gass + "です");
  console.log("ナンバーは" + cars.num + "です");
 
